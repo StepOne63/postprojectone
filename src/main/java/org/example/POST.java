7a1660ef8dcd19bfbb7e4432022ae53a46ca930e
Step X <stepan170297@gmail.com>
2024-01-09 16:02:15 +0400

    New Commit

---
+++ src/main/java/org/example/POST.java
@@ -0,0 +1,15 @@
+package org.example;
+
+public class POST {
+    public FormDate birthday;
+    public String name;
+    public String passport;
+
+    public String patronymic;
+
+    public String phone;
+
+    public String surname;
+
+    public String subscription;
+}
